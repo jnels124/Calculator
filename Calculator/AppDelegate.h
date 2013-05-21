0003012f331a6f859d3e55ed589066e2b75bf64f
Jesse Nelson <jnels124@msudenver.edu>
2013-05-21 14:20:41 -0600

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Jesse Nelson on 5/21/13.
+//  Copyright (c) 2013 Jesse Nelson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ CalculatorTests/CalculatorTests.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorTests.h
+//  CalculatorTests
+//
+//  Created by Jesse Nelson on 5/21/13.
+//  Copyright (c) 2013 Jesse Nelson. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CalculatorTests : SenTestCase
+
+@end
